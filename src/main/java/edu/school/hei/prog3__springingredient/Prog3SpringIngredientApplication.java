d8d68b4146fd78b0fa5772f6082274a8d2b037e3
harena-mihaja <mihaja2506@gmail.com>
2026-03-28 18:53:40 +0300

    chore: initial commit

---
+++ src/main/java/edu/school/hei/prog3__springingredient/Prog3SpringIngredientApplication.java
@@ -0,0 +1,13 @@
+package edu.school.hei.prog3__springingredient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3SpringIngredientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog3SpringIngredientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/school/hei/prog3__springingredient/Prog3SpringIngredientApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.school.hei.prog3__springingredient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3SpringIngredientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
