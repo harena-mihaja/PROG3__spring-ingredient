cf3c049a95a6f957fcd07cfcecf4b13ab76b5595
harena-mihaja <mihaja2506@gmail.com>
2026-03-29 15:57:31 +0300

    feat: add getAllDishes to DishService

--- src/main/java/edu/school/hei/prog3__springingredient/service/DishService.java
+++ src/main/java/edu/school/hei/prog3__springingredient/service/DishService.java
@@ -1,7 +1,21 @@
 package edu.school.hei.prog3__springingredient.service;
 
+import edu.school.hei.prog3__springingredient.entity.Dish;
+import edu.school.hei.prog3__springingredient.repository.DishRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DishService {
+    private final DishRepository repository;
+
+    public DishService(DishRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Dish> getAllDishes()
+    {
+        return (repository.findAllDishes());
+    }
 }
