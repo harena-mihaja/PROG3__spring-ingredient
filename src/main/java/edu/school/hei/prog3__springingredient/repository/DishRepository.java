ebdb335aacc7ed58b39149c8588232f73507cd59
harena-mihaja <mihaja2506@gmail.com>
2026-03-29 15:55:36 +0300

    feat: add findAllDish to DishRepository

---
+++ src/main/java/edu/school/hei/prog3__springingredient/repository/DishRepository.java
@@ -0,0 +1,47 @@
+package edu.school.hei.prog3__springingredient.repository;
+
+import edu.school.hei.prog3__springingredient.datasource.DataSource;
+import edu.school.hei.prog3__springingredient.entity.Dish;
+import edu.school.hei.prog3__springingredient.entity.DishTypeEnum;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class DishRepository {
+    private final DataSource dataSource;
+
+    public DishRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<Dish> findAllDishes()
+    {
+        String sql = "SELECT id, name, dish_type, selling_price FROM dish";
+        List<Dish> dishes = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection()) {
+            try(PreparedStatement ps = connection.prepareStatement(sql)){
+                try(ResultSet rs = ps.executeQuery()){
+                    while (rs.next())
+                    {
+                        Dish dish = Dish.builder()
+                                .id(rs.getInt("id"))
+                                .name(rs.getString("name"))
+                                .dishType(DishTypeEnum.valueOf(rs.getString("dish_type")))
+                                .price(rs.getDouble("selling_price"))
+                                .build();
+                        dishes.add(dish);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return (dishes);
+    }
+}
---
+++ src/main/java/edu/school/hei/prog3__springingredient/service/DishService.java
@@ -0,0 +1,7 @@
+package edu.school.hei.prog3__springingredient.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DishService {
+}
